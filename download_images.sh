937c72ddc04638c157b43fed6cd669cff644c787
Udbhavkush <ukush4@gwmail.gwu.edu>
2023-11-29 15:55:05 -0500

    bash file to download fake images created

---
+++ download_images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+download_link="https://archive.org/download/1mFakeFaces/1m_faces_00.tar"
+
+destination_folder="./downloaded_images"
+
+mkdir -p "$destination_folder"
+
+wget "$download_link" -P "$destination_folder"
+
+if [ $? -eq 0 ]; then
+    echo "Download successful!"
+else
+    echo "Download failed."
+fi
