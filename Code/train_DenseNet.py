1fb7a4e6e6c0f9ef6fb94aa8cf42171247987602
Arjun Bingly <arjunbin@gmail.com>
2023-12-03 02:39:42 -0500

    Update train_DenseNet.py to handle non-processed images

--- Code/train_DenseNet.py
+++ Code/train_DenseNet.py
@@ -87,7 +87,7 @@ class CustomDataset(data.Dataset):
         y= torch.FloatTensor(y)
         img = Image.open(file).convert('RGB')
         preprocess = v2.Compose([
-            v2.Resize(IMAGE_SIZE),
+            v2.Resize((IMAGE_SIZE, IMAGE_SIZE)),
             v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
