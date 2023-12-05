64676da5aa48661674ecce1583eb947022a68baf
Udbhavkush <ukush4@gwmail.gwu.edu>
2023-12-04 19:57:59 -0500

    Modified the bash file to install more 1mFakeFaces images

--- 1mFakeFaces_Download.sh
+++ 1mFakeFaces_Download.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-download_link="https://archive.org/download/1mFakeFaces/1m_faces_00.tar"
-
-destination_folder="./Data/downloaded_images"
-
+destination_folder="./Data/downloaded_images/1mFakeFaces"
 mkdir -p "$destination_folder"
 
-wget "$download_link" -P "$destination_folder"
+for i in {00..06}; do
+    download_link="https://archive.org/download/1mFakeFaces/1m_faces_$i.tar"
+    wget "$download_link" -P "$destination_folder"
 
-if [ $? -eq 0 ]; then
-    echo "Download successful!"
-else
-    echo "Download failed."
-fi
+    if [ $? -eq 0 ]; then
+        echo "Download of 1m_faces_$i.tar successful!"
+    else
+        echo "Download of 1m_faces_$i.tar failed."
+    fi
+done
