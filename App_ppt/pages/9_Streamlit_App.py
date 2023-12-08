18bfc92feb09f3b270776299addde394a36f4eb3
Udbhavkush <ukush4@gwmail.gwu.edu>
2023-12-08 15:53:49 -0500

    Update 9_Streamlit_App.py

--- App_ppt/pages/9_Streamlit_App.py
+++ App_ppt/pages/9_Streamlit_App.py
@@ -5,5 +5,5 @@ st.title('Streamlit Application')
 st.markdown('''
     - We created one application where we classify the face images into real and fake.
     - Incorporated multiple models trained on different datasets.
-    - Here's the demo of it: [FaceAuth]()
+    - Here's the demo of it: [FaceAuth](http://44.204.123.55:8888/)
 ''')
